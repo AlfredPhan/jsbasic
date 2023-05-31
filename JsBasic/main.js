7968979bb2e6f82946bc02f8633873dd539ebc11
AlfredPhan <phanminhtri0398@gmail.com>
2023-05-31 17:22:08 +0700

    new up

---
+++ JsBasic/main.js
@@ -0,0 +1,23 @@
+// 1. Đối tượng
+// -Chưa từng học Javascript
+// - Đã học, muốn ôn lại
+// -Đã học, đã bỏ cuộc, muốn học lại
+// 2. Lộ trình
+// -Giới thiệu + Lời nhắc
+// -Chuẩn bị công cụ
+// -Làm quen ngôn ngữ, thư viện, FW,..
+// - Khái niệm cơ bản
+// – Đi sâu vào những khái niệm quan trọng
+// – Thực hành làm 1 project
+// 3. Lời nhắc
+// -Các thuật ngữ tiếng Anh
+// -Trình bày khi viết code
+// – Thực hành sau mỗi bài học
+// -Cố hiểu quan trọng hơn cố nhớ
+// - Vạch rõ mục tiêu, theo đuổi đến cùng//
+
+var name = 'Tri';
+var age = 23
+alert(name);
+alert(age);
+
